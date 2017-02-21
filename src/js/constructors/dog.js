9822062a9bcfca4ff9f1a2a4f933207a5722973b
Cory Vaughan <codercory13@gmail.com>
2017-02-21 17:51:24 -0500

    Normal mode complete.

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,10 @@
-function Dog () {
+function Dog (dogs) {
+  var dogs = dogs || {};
+  this.color = dogs.color;
+  this.hungry = (dogs.hungry == undefined) ? true : false;
+  this.status = dogs.status || 'normal';
 }
+
+
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,4 +1,15 @@
-function Person () {
+function Person (people) {
+  var people = people || {};
+  this.cool = (people.cool == undefined) ? false : true;
+    };
+
+Person.prototype.pet = function (petted) {
+  petted.status = 'happy'
 };
+
+Person.prototype.feed = function (hungryDog){
+  hungryDog.hungry = false
+};
+
 
 export { Person };
